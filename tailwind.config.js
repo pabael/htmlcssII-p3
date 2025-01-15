5344aeb6e80f0c1433298b7cf00b32521833d825
[Paloma Balmori Elosegui] <[pbalmorie@uoc.edu]>
2025-01-15 19:07:39 +0100

    article

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#014452',
+        'highlight': '#1e7268',
+        'background': '#c6e8ee',
+        'card-bg': '#c6e8ee',
+        'primary': 'hwb(220 30% 20%)',
+      },
+      fontFamily: {
+        sans: [
+          '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto',
+          '"Helvetica Neue"', 'Arial', 'sans-serif', 
+          '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"'
+        ],
+        title: ['"Arima"'],
+      }
+    },
   },
   plugins: [],
 }
